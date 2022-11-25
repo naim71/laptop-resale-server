327e9202cc62c3a04353f83481e65781af1e8eee
naim71 <naimkhan1314@gmail.com>
2022-11-26 02:55:56 +0600

    git check

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4evolx3.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+            const categoriesCollection = client.db('denGadget').collection('categoriesList');
+
+            app.get('/categoriesList', async(req, res) =>{
+                const query = {};
+                const categories = await categoriesCollection.find(query).toArray();
+                res.send(categories);
+            })
+    }
+    finally{
+
+    }
+}
+run().catch(console.log);
+
+app.get ('/', async(req, res) =>{
+    res.send('Den Gadget is Running');
+})
+
+app.listen(port, () => console.log(`Den Gadget is running on ${port}`))
