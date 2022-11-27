b5cdec3d048b4351f582e6f5fc7c3d9d86d4a45f
naim71 <naimkhan1314@gmail.com>
2022-11-27 20:52:50 +0600

    role query

--- index.js
+++ index.js
@@ -59,7 +59,19 @@ async function run(){
                 res.send(result);
 
             })
+            app.get('/users', async(req, res) =>{
+                let query = {};
+                if (req.query.role) {
+                    query = {
+                        role: req.query.role
+                    }
+                }
+                const result = await usersCollection.find(query).toArray();
+                res.send(result);
+            })
 
+          
+    
 
 
 
