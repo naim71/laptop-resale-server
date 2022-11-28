2ad43cac1a20783a0860eca5c5ee19512d20d3bc
naim71 <naimkhan1314@gmail.com>
2022-11-28 22:26:38 +0600

    final push

--- index.js
+++ index.js
@@ -142,6 +142,12 @@ async function run() {
             const result = await bookingsCollection.find(query).toArray();
             res.send(result);
         })
+        app.get('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await bookingsCollection.findOne(query);
+            res.send(result);
+        })
 
         // app.get('/bookings/:id', async (req, res) => {
         //     const id = req.params.id;
